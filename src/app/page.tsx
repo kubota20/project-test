dbb48a5410fcbe9ca08ec15b22836d3449268406
kubota20 <doc0000doc77@gmail.com>
2024-12-29 17:08:02 +0900

    ホームページ作成、三目並べ追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>練習用</h1>
+"use client";
 
-      <ul>
-        <li></li>
-      </ul>
+import Button from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+  return (
+    <div className="flex flex-col items-center justify-between">
+      <h1 className="font-bold text-3xl my-24">練習用</h1>
+
+      <div className="w-5/6 border border-black shadow p-3">
+        <div className="text-center">
+          <h2 className="font-bold text-2xl my-6">ゲーム</h2>
+          <Button
+            className="mt-8 mb-6"
+            onClick={() => router.push("tic-tac-toe")}
+          >
+            三目並べ
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
