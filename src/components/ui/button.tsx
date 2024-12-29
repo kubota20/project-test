d99abd369d40890fc00a970aa735bf74d184945f
kubota20 <doc0000doc77@gmail.com>
2024-12-29 17:09:15 +0900

    button ui 追加

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,28 @@
+import React, { forwardRef } from "react";
+import { cn } from "@/lib/utils";
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, disabled, type = "button", ...props }, ref) => {
+    return (
+      <button
+        className={cn(
+          `w-auto text-white rounded-full bg-black border-transparent px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50 transition hover:bg-slate-700`,
+          className
+        )}
+        disabled={disabled}
+        type={type}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+Button.displayName = "Button";
+
+export default Button;
