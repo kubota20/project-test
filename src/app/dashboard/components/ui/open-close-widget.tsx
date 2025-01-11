af6ccaec840a81302de07ea581029afc4603525a
kubota20 <doc0000doc77@gmail.com>
2025-01-12 03:38:07 +0900

    toggleSidebarは640px以下だと追加されないためもう一つonClickイベントをスマホ用に追加

--- src/app/dashboard/components/ui/open-close-widget.tsx
+++ src/app/dashboard/components/ui/open-close-widget.tsx
@@ -3,16 +3,25 @@
 import { SidebarTrigger, useSidebar } from "@/components/ui/sidebar";
 
 export const OpenCloseWidget = () => {
-  const { open, setOpen } = useSidebar();
+  const { toggleSidebar, open, openMobile, isMobile, setOpen } = useSidebar();
+
+  const isOpen = isMobile ? openMobile : open; // デバイスに応じた状態を取得
+
   return (
-    <div
-      className="flex items-center justify-center"
-      onClick={() => setOpen(open)}
-    >
-      <SidebarTrigger />
-      <p className={`text-sm cursor-pointer max-sm:hidden`}>
-        {open ? "閉じる" : "開く"}
-      </p>
-    </div>
+    <>
+      {/* 640px以上で表示 */}
+      <div
+        className="flex items-center justify-center max-sm:hidden"
+        onClick={toggleSidebar}
+      >
+        <SidebarTrigger />
+        <p className="text-sm cursor-pointer ">{isOpen ? "閉じる" : "開く"}</p>
+      </div>
+
+      {/* 640px以下で表示 */}
+      <div className="sm:hidden" onClick={() => setOpen(open)}>
+        <SidebarTrigger />
+      </div>
+    </>
   );
 };
