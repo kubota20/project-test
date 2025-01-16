a87fa35838a6f4330588c4cc90b95c4177b3068e
kubota20 <doc0000doc77@gmail.com>
2025-01-16 21:19:37 +0900

    レスポンシブスタイル変更しました

--- src/app/dashboard/components/chart/pie-charts.tsx
+++ src/app/dashboard/components/chart/pie-charts.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { PieChart, Pie, ResponsiveContainer } from "recharts";
 
 const data01 = [
@@ -21,6 +24,30 @@ const data02 = [
 ];
 
 export const PieCharts = () => {
+  const [radius, setRadius] = useState({
+    inner: 70,
+    outer: 60,
+    outer2: 90,
+    label: false,
+  });
+
+  useEffect(() => {
+    const updateRadius = () => {
+      const width = window.innerWidth;
+      setRadius({
+        inner: width < 640 ? 50 : width < 1024 ? 60 : 70,
+        outer: width < 640 ? 40 : width < 1024 ? 50 : 60,
+        outer2: width < 640 ? 60 : width < 1024 ? 80 : 90,
+        //  1024以下でラベル非表示
+        label: width < 1024 == false,
+      });
+    };
+
+    window.addEventListener("resize", updateRadius);
+    updateRadius(); // 初回実行
+
+    return () => window.removeEventListener("resize", updateRadius);
+  }, []);
   return (
     <ResponsiveContainer width="100%" height="100%">
       <PieChart width={400} height={400}>
@@ -29,7 +56,7 @@ export const PieCharts = () => {
           dataKey="value"
           cx="50%"
           cy="50%"
-          outerRadius={60}
+          outerRadius={radius.outer}
           fill="#60a5fa"
         />
         <Pie
@@ -37,10 +64,10 @@ export const PieCharts = () => {
           dataKey="value"
           cx="50%"
           cy="50%"
-          innerRadius={70}
-          outerRadius={90}
+          innerRadius={radius.inner}
+          outerRadius={radius.outer2}
           fill="#2563eb"
-          label
+          label={radius.label}
         />
       </PieChart>
     </ResponsiveContainer>
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -21,10 +21,10 @@ const DashboardPage = () => {
   };
 
   return (
-    <div className="mt-6 py-6 px-8 space-y-4">
+    <div className="mt-1 py-3 px-4 sm:mt-6 sm:py-6 sm:px-8 space-y-4">
       {/* ユーザー数の推移 */}
       <div className="border-2 p-2">
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between max-md:flex-col">
           <p className="text-gray-500 text-sm">ユーザー数の推移</p>
 
           {/*　年月日 選択  */}
@@ -34,18 +34,18 @@ const DashboardPage = () => {
         </div>
 
         {/* ユーザーチャート */}
-        <div className="w-full h-96">
+        <div className="w-full h-52 sm:h-60 md:h-96 ">
           <UserChart data={chartData} />
         </div>
       </div>
 
-      <div className="flex  items-center justify-center space-x-4">
-        {/* 円グラフ */}
-        <div className="w-full h-72 border-2 p-2">
+      {/* 円グラフ */}
+      <div className="flex items-center justify-center space-x-4">
+        <div className="w-full h-52 sm:h-72 border-2 p-2">
           <p className="text-gray-500 text-sm">円グラフ１</p>
           <PieCharts />
         </div>
-        <div className="w-full h-72 border-2 p-2">
+        <div className="w-full h-52 sm:h-72 border-2 p-2">
           <p className="text-gray-500 text-sm">円グラフ2</p>
           <PieCharts />
         </div>
