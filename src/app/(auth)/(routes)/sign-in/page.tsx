543740c105631f51814538fd268643deb679ff72
kubota20 <doc0000doc77@gmail.com>
2025-01-20 19:34:31 +0900

    itHubでのログイン初期作成

---
+++ src/app/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const SignInPage = () => {
+  return (
+    <div>
+      <Button>GitHubでのログイン</Button>
+    </div>
+  );
+};
+
+export default SignInPage;
