bab06735063749ef64977eee5c3a2d2394eb2e7f
kubota20 <doc0000doc77@gmail.com>
2025-01-20 19:35:08 +0900

    authOptions　初期作成

---
+++ src/lib/auth.ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+
+export const authOptions = {
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
