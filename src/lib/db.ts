1898306268e43b37393142592e2ebbe74c8602c8
kubota20 <doc0000doc77@gmail.com>
2025-01-22 18:01:07 +0900

    prismaでデータベース操作するので   neondatabase/serverlessは削除

--- src/lib/db.ts
+++
@@ -1,8 +0,0 @@
-import { neon } from "@neondatabase/serverless";
-
-export async function getData() {
-  const sql = neon(process.env.DATABASE_URL as string);
-  const response = await sql`SELECT version()`;
-
-  return response[0].version;
-}
