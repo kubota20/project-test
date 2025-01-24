14c1690301434f3ed453396c739beca5bd12e470
kubota20 <doc0000doc77@gmail.com>
2025-01-24 21:36:44 +0900

    修正

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,8 @@
 // Prisma Clientのインスタンスを格納
-
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-const globalForPrisma = global as unknown as { prisma: typeof prisma };
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export default prisma;
