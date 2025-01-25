aecc01ecf65af5593002b9d6c6d945fb655bb489
kubota20 <doc0000doc77@gmail.com>
2025-01-25 16:08:55 +0900

    next-authのオプションを追加、現在特定のメールアドレス以外拒否

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,59 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
+
+// GitHub認証
 import GitHub from "next-auth/providers/github";
 
-export const authOptions = {
+// prisma
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GitHub({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET, // セッション暗号化用の秘密キー
+  session: {
+    strategy: "jwt", // セッション管理方式 (JWTベース)
+  },
+
+  callbacks: {
+    async signIn({ user }) {
+      const allowedEmails = [process.env.GitHub_EMAIL as string]; // 特定のメールアドレスを許可
+      if (user.email && allowedEmails.includes(user.email)) {
+        return true; // サインイン許可
+      }
+
+      return false; // サインイン拒否
+    },
+    async session({ session, token }) {
+      session.user.id = token.id;
+
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id; // トークンにユーザーIDを追加
+      }
+      return token;
+    },
+    async redirect({ url, baseUrl }) {
+      // デフォルトで /admin にリダイレクト
+      if (url === "/" || url === `${baseUrl}/`) {
+        return `${baseUrl}/admin`;
+      }
+      // 他のケースでは、指定されたURLにリダイレクト
+      return url.startsWith(baseUrl) ? url : baseUrl;
+    },
+  },
+
+  pages: {
+    signIn: "/sign-in",
+    error: "/error",
+  },
 };
 
 export default NextAuth(authOptions);
