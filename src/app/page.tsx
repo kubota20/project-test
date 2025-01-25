82f73a9a42c1634a6ffc866cd77db783f860c89e
kubota20 <doc0000doc77@gmail.com>
2025-01-25 16:07:33 +0900

    signInボタンを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import { signIn } from "next-auth/react";
 
 export default function Home() {
   const router = useRouter();
@@ -76,10 +77,10 @@ export default function Home() {
           <div className="text-center">
             <h2 className="font-bold text-2xl my-2">管理者用</h2>
             <p className="text-sm text-gray-500">管理者しか使用できません</p>
-            <div className=" space-x-4">
+            <div className="space-x-4">
               <Button
                 className="mt-8 text-xs font-bold p-2"
-                onClick={() => router.push("/sign-in")}
+                onClick={() => signIn("github")}
               >
                 ログイン
               </Button>
