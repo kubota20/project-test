5b8233b15f16e538a92dc02ca5dea7af810c21ff
kubota20 <doc0000doc77@gmail.com>
2025-01-25 16:07:59 +0900

    テスト

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,12 @@
+"use client";
+import { signOut } from "next-auth/react";
+
 const AdminPage = () => {
-  return <div>AdminPage</div>;
+  return (
+    <div>
+      <button onClick={() => signOut()}>ログアウト</button>
+    </div>
+  );
 };
 
 export default AdminPage;
